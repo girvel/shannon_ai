0d6c6e16313e2a104161b9086e0d7c6309bd50f4
girvel <widauka@ya.ru>
2022-11-30 23:46:01 +0400

    Structures and pieces

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod piece {
+    const NONE: i8 = 0;
+    const PAWN: i8 = 1;
+    const KNIGHT: i8 = 2;
+    const BISHOP: i8 = 3;
+    const ROOK: i8 = 4;
+    const QUEEN: i8 = 5;
+    const KING: i8 = 6;
+}
+
+struct Position {
+    board: [i8; 64],
+    side_to_play: i8,
+}
+
+struct Move {
+    from: usize,
+    to: usize,
+    promotion: i8,
+}
+
 fn main() {
     println!("Hello, world!");
 }
