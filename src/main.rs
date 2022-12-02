d87cdebebb06d418ed1e591ade5c594ed765a303
girvel <widauka@ya.ru>
2022-12-02 16:43:46 +0400

    Starting position and basic display

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod piece {
-    const NONE: i8 = 0;
-    const PAWN: i8 = 1;
-    const KNIGHT: i8 = 2;
-    const BISHOP: i8 = 3;
-    const ROOK: i8 = 4;
-    const QUEEN: i8 = 5;
-    const KING: i8 = 6;
+    pub const NONE: i8 = 0;
+    pub const PAWN: i8 = 1;
+    pub const KNIGHT: i8 = 2;
+    pub const BISHOP: i8 = 3;
+    pub const ROOK: i8 = 4;
+    pub const QUEEN: i8 = 5;
+    pub const KING: i8 = 6;
 }
 
+#[derive(Debug)]
 struct Position {
     board: [i8; 64],
     side_to_play: i8,
@@ -19,6 +20,24 @@ struct Move {
     promotion: i8,
 }
 
+impl Position {
+    fn starting_position() -> Position {
+        Position {
+            board: [
+                piece::ROOK, piece::KNIGHT, piece::BISHOP, piece::QUEEN, piece::KING, piece::BISHOP, piece::KNIGHT, piece::ROOK,
+                piece::PAWN, piece::PAWN, piece::PAWN, piece::PAWN, piece::PAWN, piece::PAWN, piece::PAWN, piece::PAWN,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                -piece::PAWN, -piece::PAWN, -piece::PAWN, -piece::PAWN, -piece::PAWN, -piece::PAWN, -piece::PAWN, -piece::PAWN,
+                -piece::ROOK, -piece::KNIGHT, -piece::BISHOP, -piece::QUEEN, -piece::KING, -piece::BISHOP, -piece::KNIGHT, -piece::ROOK,
+            ],
+            side_to_play: 1,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", Position::starting_position());
 }
