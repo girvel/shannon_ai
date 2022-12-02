72478a729c39c06585d2ad45c6fd918a198620c7
girvel <widauka@ya.ru>
2022-12-03 02:32:56 +0400

    Improve code style

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-const BOARD_SIZE: usize = 8;
+pub const BOARD_SIZE: usize = 8;
 
-mod pieces {
+pub mod pieces {
     pub const NONE: i8 = 0;
     pub const PAWN: i8 = 1;
     pub const KNIGHT: i8 = 2;
@@ -10,19 +10,24 @@ mod pieces {
     pub const KING: i8 = 6;
 }
 
+pub mod sides {
+    pub const WHITE: i8 = 1;
+    pub const BLACK: i8 = -1;
+}
+
 #[derive(Debug, Copy, Clone)]
-struct Position {
+pub struct Position {
     board: [i8; 64],
     side_to_play: i8,
 }
 
-struct Move {
+pub struct Move {
     from: usize,
     to: usize,
     promotion: i8,
 }
 
-fn to_coordinates(index: usize) -> (usize, usize) {
+fn coordinates_from_index(index: usize) -> (usize, usize) {
     return (index % BOARD_SIZE, index / BOARD_SIZE)
 }
 
@@ -30,28 +35,26 @@ impl Position {
     fn starting_position() -> Position {
         Position {
             board: [
-                pieces::ROOK, pieces::KNIGHT, pieces::BISHOP, pieces::QUEEN, pieces::KING, pieces::BISHOP, pieces::KNIGHT, pieces::ROOK,
-                pieces::PAWN, pieces::PAWN, pieces::PAWN, pieces::PAWN, pieces::PAWN, pieces::PAWN, pieces::PAWN, pieces::PAWN,
-                0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0,
-                -pieces::PAWN, -pieces::PAWN, -pieces::PAWN, -pieces::PAWN, -pieces::PAWN, -pieces::PAWN, -pieces::PAWN, -pieces::PAWN,
+                 pieces::ROOK,  pieces::KNIGHT,  pieces::BISHOP,  pieces::QUEEN,  pieces::KING,  pieces::BISHOP,  pieces::KNIGHT,  pieces::ROOK,
+                 pieces::PAWN,  pieces::PAWN,    pieces::PAWN,    pieces::PAWN,   pieces::PAWN,  pieces::PAWN,    pieces::PAWN,    pieces::PAWN,
+                 pieces::NONE,  pieces::NONE,    pieces::NONE,    pieces::NONE,   pieces::NONE,  pieces::NONE,    pieces::NONE,    pieces::NONE,
+                 pieces::NONE,  pieces::NONE,    pieces::NONE,    pieces::NONE,   pieces::NONE,  pieces::NONE,    pieces::NONE,    pieces::NONE,
+                 pieces::NONE,  pieces::NONE,    pieces::NONE,    pieces::NONE,   pieces::NONE,  pieces::NONE,    pieces::NONE,    pieces::NONE,
+                 pieces::NONE,  pieces::NONE,    pieces::NONE,    pieces::NONE,   pieces::NONE,  pieces::NONE,    pieces::NONE,    pieces::NONE,
+                -pieces::PAWN, -pieces::PAWN,   -pieces::PAWN,   -pieces::PAWN,  -pieces::PAWN, -pieces::PAWN,   -pieces::PAWN,   -pieces::PAWN,
                 -pieces::ROOK, -pieces::KNIGHT, -pieces::BISHOP, -pieces::QUEEN, -pieces::KING, -pieces::BISHOP, -pieces::KNIGHT, -pieces::ROOK,
             ],
-            side_to_play: 1,
+            side_to_play: sides::WHITE,
         }
     }
 }
 
 impl Move {
-    fn apply(&self, position: Position) -> Position {
-        let mut position = position;
-
+    fn apply(&self, mut position: Position) -> Position {
         let piece = position.board[self.from];
         position.board[self.from] = pieces::NONE;
 
-        let (x, y) = to_coordinates(self.from);
+        let (_, y) = coordinates_from_index(self.from);
 
         if (piece == pieces::PAWN && y == 6) || (piece == -pieces::PAWN && y == 1) {
             position.board[self.to] = self.promotion;
